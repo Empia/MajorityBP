29ad820b48efa1aca2edd652e8dfc5141a0ff620
Stanislav <iamjacke@gmail.com>
2014-03-04 06:05:45 +0600

    Template for ElementTracer

--- bp-core/src/main/scala/utils/ElementsTracer.scala
+++ bp-core/src/main/scala/utils/ElementsTracer.scala
@@ -1,6 +1,16 @@
 package main.scala.utils
 
 trait ElementTracer {
-  // Get All SubBricks for Brick
-  // Get All Value of SubBrick and Brick (Allow value)
+  // block
+  // brick
+  // subbrick
+  // brick(subbricks)
+
+  // Value validation
+  // All Value of SubBrick and Brick (Allowed value)
 }
+
+// def register[Type](element_type, element_name, description)
+// def register_value(element_type, element_name, allowed_value)
+
+// value types
